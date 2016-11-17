874b0a52d8e48d4aaa299333f3803e5998d40c70
rxso <rajat.soni@icloud.com>
2016-11-16 21:44:11 -0600

    Change quote length

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,4 +1,4 @@
 class Quote < ActiveRecord::Base
-  validates :saying, presence: true, length: {maximum: 200, minimum: 20 }
+  validates :saying, presence: true, length: {maximum: 250, minimum: 20 }
   validates :author, presence: true, length: {maximum: 50, minimum: 3 }
 end
